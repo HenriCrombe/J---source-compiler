662bb5c48cb1b2ee03d32af1d008694233f48db2
HenriCrombe <henri.crombe@student.uclouvain.be>
2015-02-09 15:44:33 +0100

    First commit / push Divide operator has been implemented. There is still a bug with the division of 123/3 = 42.33333 -> TODO

---
+++ j--/tests/pass/Division.java
@@ -0,0 +1,11 @@
+// Copyright 2013 Bill Campbell, Swami Iyer and Bahar Akbal-Delibas
+
+package pass;
+
+import java.lang.System;
+
+public class Division{
+	public int divide(int x,int y){
+		return x / y;
+	}
+}
